c3f59b45edfdc5572e0711f9c44e85457dc0fb27
Weslley Almeida <weslleyalmeida6@gmail.com>
2023-09-22 19:36:33 -0300

    web

---
+++ web/src/app.tsx
@@ -0,0 +1,124 @@
+import { PromptSelect } from "@/components/prompt-select";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Separator } from "@/components/ui/separator";
+import { Slider } from "@/components/ui/slider";
+import { Textarea } from "@/components/ui/textarea";
+import { VideoInputForm } from "@/components/video-input-form";
+import { useCompletion } from "ai/react";
+import { Github, Wand2 } from 'lucide-react';
+import { useState } from "react";
+
+export function App() {
+  const [temperature, setTemperature] = useState(0.5)
+  const [videoId, setVideoId] = useState<string | null>(null)
+
+  const { input, setInput, handleInputChange, handleSubmit, completion, isLoading } = useCompletion({
+    api: 'http://localhost:3333/ai/complete',
+    body: {
+      videoId,
+      temperature,
+    },
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  })
+
+  return (
+    <div className="min-h-screen flex flex-col" >
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>
+
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">
+            Desenvolvido com ❤️ no NLW da RocketSeat
+          </span>
+          <Separator orientation="vertical" className="h-6" />
+          <Button variant={"outline"}>
+            <Github className="w-4 h-4 mr-2" />
+            Github
+          </Button>
+        </div>
+      </div>
+
+      <main className="flex-1 p-6 flex gap-6">
+        <div className="flex flex-1 flex-col gap-4">
+          <div className="grid grid-rows-2 gap-4 flex-1">
+            <Textarea
+              className="resize-none p-4 leading-relaxed"
+              placeholder="Inclua o prompt para a IA..."
+              value={input}
+              onChange={handleInputChange}
+            />
+            <Textarea
+              className="resize-none p-4 leading-relaxed"
+              placeholder="Resultado gerado pela IA..."
+              readOnly
+              value={completion}
+            />
+          </div>
+          <p className="text-sm text-muted-foreground">
+            Lembre-se: você pode utilizar a variável <code className="text-violet-400">{'{transcription}'}</code> no seu prompt para adicionar o conteúdo da transcrição do vídeo selecionado.
+          </p>
+        </div>
+        <aside className="w-80 space-y-6">
+          <VideoInputForm onVideoUploaded={setVideoId} />
+
+          <Separator />
+
+          <form className="space-y-6" onSubmit={handleSubmit}>
+            <div className="space-y-2">
+              <Label>Prompt</Label>
+
+              <PromptSelect onPromptSelected={setInput} />
+            </div>
+
+            <Separator />
+
+            <div className="space-y-2">
+              <Label>Modelo</Label>
+
+              <Select defaultValue="gpt3.5" disabled>
+                <SelectTrigger>
+                  <SelectValue></SelectValue>
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="gpt3.5">GPT 3.5-Turbo 16k</SelectItem>
+                </SelectContent>
+              </Select>
+              <span className="block text-xs text-muted-foreground italic">
+                Você podera customizar essa opção em breve.
+              </span>
+            </div>
+
+            <Separator />
+
+            <div className="space-y-4">
+              <Label>Temperatura</Label>
+
+              <Slider
+                min={0}
+                max={1}
+                step={0.1}
+                value={[temperature]}
+                onValueChange={value => setTemperature(value[0])}
+              />
+              <span className="block text-xs text-muted-foreground italic leading-relaxed">
+                Valores mais altos tendem a gerar resultados mais criativos e com possíveis erros.
+              </span>
+            </div>
+
+            <Separator />
+
+            <Button type="submit" className="w-full" disabled={isLoading}>
+              Executar
+              <Wand2 className="w-4 h-4 ml-2" />
+            </Button>
+
+          </form>
+        </aside>
+      </main>
+    </div >
+  )
+}
---
+++ web/src/components/video-input-form.tsx
@@ -0,0 +1,158 @@
+import { Button } from "@/components/ui/button"
+import { Label } from "@/components/ui/label"
+import { Separator } from "@/components/ui/separator"
+import { Textarea } from "@/components/ui/textarea"
+import { api } from "@/lib/axios"
+import { getFFmpeg } from "@/lib/ffmpeg"
+import { fetchFile } from "@ffmpeg/util"
+import { FileVideo, Upload } from "lucide-react"
+import { ChangeEvent, FormEvent, useMemo, useRef, useState } from "react"
+
+type Status = 'waiting' | 'converting' | 'uploading' | 'generating' | 'success'
+
+const statusMessages = {
+  converting: 'Convertendo...',
+  generating: 'Transcrevendo...',
+  uploading: 'Carregando...',
+  success: 'Sucesso!',
+}
+
+interface VideoInputFormProps {
+  onVideoUploaded: (videoId: string) => void
+}
+
+export function VideoInputForm({ onVideoUploaded }: VideoInputFormProps) {
+  const [videoFile, setVideoFile] = useState<File | null>(null)
+  const [status, setStatus] = useState<Status>('waiting')
+  const promptInputRef = useRef<HTMLTextAreaElement>(null)
+
+  function handleFileSelected(event: ChangeEvent<HTMLInputElement>) {
+    const { files } = event.currentTarget
+
+    if (!files) return
+
+    const selectedFile = files[0]
+
+    setVideoFile(selectedFile)
+  }
+
+  async function convertVideoToAudio(video: File) {
+    console.log('Convert started.')
+
+    const ffmpeg = await getFFmpeg()
+
+    await ffmpeg.writeFile('input.mp4', await fetchFile(video))
+    // ffmpeg.on('log', log => console.log(log))
+
+    ffmpeg.on('progress', progress => {
+      console.log('Convert progress: ' + Math.round(progress.progress * 100))
+    })
+
+    await ffmpeg.exec(
+      [
+        '-i', 'input.mp4',
+        '-map', '0:a',
+        '-b:a', '20k',
+        '-acodec', 'libmp3lame',
+        'output.mp3'
+      ]
+    )
+
+    const data = await ffmpeg.readFile('output.mp3');
+
+    const audioFileBlob = new Blob([data], { type: 'audio/mpeg' })
+    const audioFile = new File([audioFileBlob], 'audio.mp3', { type: 'audio/mpeg' })
+
+    console.log('Convert finished.')
+
+    return audioFile
+
+  }
+
+  async function handleUploadVideo(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault()
+
+    const prompt = promptInputRef.current?.value
+
+    if (!videoFile) return
+
+    setStatus('converting')
+
+    const audioFile = await convertVideoToAudio(videoFile)
+
+    const data = new FormData()
+
+    data.append('file', audioFile)
+
+    setStatus('uploading')
+
+    const response = await api.post('/videos', data)
+
+    const videoId = response.data.video.id
+
+    setStatus('generating')
+
+    await api.post(`/videos/${videoId}/transcription`, {
+      prompt,
+    })
+
+    setStatus('success')
+
+    onVideoUploaded(videoId)
+  }
+
+  const previewUrl = useMemo(() => {
+    if (!videoFile) return null
+
+    return URL.createObjectURL(videoFile)
+  }, [videoFile])
+
+  return (
+    <form className="space-y-6" onSubmit={handleUploadVideo}>
+      <label
+        htmlFor="video"
+        className="relative border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
+      >
+        {previewUrl ? (
+          <video src={previewUrl} controls={false} className="pointer-events-none absolute inset-0" />
+        ) : (
+          <>
+            <FileVideo className="w-4 h-4" />
+            Selecione um vídeo
+          </>
+        )}
+      </label>
+
+      <input type="file" id="video" accept="video/mp4" className="sr-only" onChange={handleFileSelected} />
+
+      <Separator />
+
+      <div className="space-y-2">
+        <Label htmlFor="transcription_prompt">Prompt de transcrição</Label>
+        <Textarea
+          disabled={status != 'waiting'}
+          ref={promptInputRef}
+          id="transcription_prompt"
+          className="h-24 leading-relaxed resize-none"
+          placeholder="Inclua palavras-chave mencionadas no vídeo separadas por vírgula."
+        />
+
+        <Button
+          data-success={status === 'success'}
+          disabled={status != 'waiting'}
+          type="submit"
+          className="w-full data-[success=true]:bg-emerald-400"
+        >
+          {status === 'waiting' ? (
+            <>
+              Carregar vídeo
+              <Upload className="w-4 h-4 ml-2" />
+            </>
+          ) : (
+            statusMessages[status]
+          )}
+        </Button>
+      </div>
+    </form>
+  )
+}
